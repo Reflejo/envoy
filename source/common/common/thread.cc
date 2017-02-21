baec0090cc227f9e066746685089f4fb35b942c4
Martin Conte Mac Donell <martin@lyft.com>
2017-02-21 00:51:07 -0800

    Make currentThreadId multiplatform

--- source/common/common/thread.cc
+++ source/common/common/thread.cc
@@ -1,7 +1,13 @@
 #include "assert.h"
 #include "thread.h"
 
+#ifdef linux
 #include <sys/syscall.h>
+#elif defined(__FreeBSD__)
+#include <pthread_np.h>
+#elif defined(__APPLE__)
+#include <mach/mach.h>
+#endif
 
 namespace Thread {
 
@@ -14,7 +20,17 @@ Thread::Thread(std::function<void()> thread_routine) : thread_routine_(thread_ro
   UNREFERENCED_PARAMETER(rc);
 }
 
-int32_t Thread::currentThreadId() { return syscall(SYS_gettid); }
+int32_t Thread::currentThreadId() {
+#ifdef linux
+  return syscall(SYS_gettid);
+#elif defined(__APPLE__)
+  int ret = mach_thread_self();
+  mach_port_deallocate(mach_task_self(), ret);
+  return ret;
+#elif defined(__FreeBSD__)
+  return pthread_getthreadid_np();
+#endif
+}
 
 void Thread::join() {
   int rc = pthread_join(thread_id_, nullptr);
